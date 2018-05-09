e8c8262cb78555784a1f624178a4cb5091883027
V Harshith <v.harshith@ad.infosys.com>
2018-05-09 18:04:39 +0530

    version-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+//import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,8 +8,10 @@ import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [ AppComponent, WelcomeComponent ],
-  imports: [ BrowserModule.withServerTransition({ appId: 'universal-cli' }), AppRoutingModule ],
+  imports: [ BrowserModule.withServerTransition({ appId: 'universal-cli' }), 
+            AppRoutingModule ],
   providers: [],
+  // schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
